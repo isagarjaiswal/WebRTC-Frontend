a7ce1ddac12c692fe34379cdcbbc3f92c3b66310
isagarjaiwal <sagarjaiswal815555@gmail.com>
2024-03-12 23:59:36 +0530

    add server deploy link

--- src/ws.js
+++ src/ws.js
@@ -1,4 +1,5 @@
 import socketIOClient from "socket.io-client";
 
-const WS = "http://localhost:8080";
+// const WS = "http://localhost:8080";
+const WS = "https://webrtc-backend-1.onrender.com";
 export const ws = socketIOClient(WS);
