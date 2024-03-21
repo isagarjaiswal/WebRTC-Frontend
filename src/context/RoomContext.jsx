340e73ab546b06237c99f1067272058c5f3191d5
isagarjaiwal <sagarjaiswal815555@gmail.com>
2024-03-21 17:57:13 +0530

    fix username bugs

--- src/context/RoomContext.jsx
+++ src/context/RoomContext.jsx
@@ -28,13 +28,13 @@ import {
 import { useNavigate } from "react-router-dom";
 import Peer from "peerjs";
 import { ws } from "../ws";
-import { peersReducer} from "../reducers/peerReducer";
+import { peersReducer } from "../reducers/peerReducer";
 import {
   addPeerStreamAction,
   addPeerNameAction,
   removePeerStreamAction,
   addAllPeersAction,
-} from "../reducers/peerAction"
+} from "../reducers/peerAction";
 import { UserContext } from "./UserContext";
 
 export const RoomContext = createContext({
@@ -56,149 +56,143 @@ export const RoomProvider = ({ children }) => {
   const [roomId, setRoomId] = useState("");
 
   const enterRoom = ({ roomId }) => {
-      navigate(`/room/${roomId}`);
+    navigate(`/room/${roomId}`);
   };
-  const getUsers = ({
-      participants,
-  }) => {
-      dispatch(addAllPeersAction(participants));
+  const getUsers = ({ participants }) => {
+    dispatch(addAllPeersAction(participants));
   };
 
   const removePeer = (peerId) => {
-      dispatch(removePeerStreamAction(peerId));
+    dispatch(removePeerStreamAction(peerId));
   };
 
   const switchStream = (stream) => {
-      setScreenSharingId(me?.id || "");
-      Object.values(me?.connections).forEach((connection) => {
-          const videoTrack = stream
-              ?.getTracks()
-              .find((track) => track.kind === "video");
-          connection[0].peerConnection
-              .getSenders()
-              .find((sender) => sender.track.kind === "video")
-              .replaceTrack(videoTrack)
-              .catch((err) => console.error(err));
-      });
+    setScreenSharingId(me?.id || "");
+    Object.values(me?.connections).forEach((connection) => {
+      const videoTrack = stream
+        ?.getTracks()
+        .find((track) => track.kind === "video");
+      connection[0].peerConnection
+        .getSenders()
+        .find((sender) => sender.track.kind === "video")
+        .replaceTrack(videoTrack)
+        .catch((err) => console.error(err));
+    });
   };
 
   const shareScreen = () => {
-      if (screenSharingId) {
-          navigator.mediaDevices
-              .getUserMedia({ video: true, audio: true })
-              .then(switchStream);
-      } else {
-          navigator.mediaDevices.getDisplayMedia({}).then((stream) => {
-              switchStream(stream);
-              setScreenStream(stream);
-          });
-      }
+    if (screenSharingId) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true })
+        .then(switchStream);
+    } else {
+      navigator.mediaDevices.getDisplayMedia({}).then((stream) => {
+        switchStream(stream);
+        setScreenStream(stream);
+      });
+    }
   };
 
-  const nameChangedHandler = ({
-      peerId,
-      userName,
-  }) => {
-      dispatch(addPeerNameAction(peerId, userName));
+  const nameChangedHandler = ({ peerId, userName }) => {
+    dispatch(addPeerNameAction(peerId, userName));
   };
 
   useEffect(() => {
-      ws.emit("change-name", { peerId: userId, userName, roomId });
+    ws.emit("change-name", { peerId: userId, userName, roomId });
   }, [userName, userId, roomId]);
 
   useEffect(() => {
-      // const peer = new Peer(userId, {
-      //     host: "peerjs.webrtctest.online",
-      //     path: "/",
-      // });
-      const peer = new Peer(userId);
+    // const peer = new Peer(userId, {
+    //     host: "peerjs.webrtctest.online",
+    //     path: "/",
+    // });
+    const peer = new Peer(userId);
 
-      setMe(peer);
+    setMe(peer);
 
-      try {
-          navigator.mediaDevices
-              .getUserMedia({ video: true, audio: true })
-              .then((stream) => {
-                  setStream(stream);
-              });
-      } catch (error) {
-          console.error(error);
-      }
+    try {
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true })
+        .then((stream) => {
+          setStream(stream);
+        });
+    } catch (error) {
+      console.error(error);
+    }
 
-      ws.on("room-created", enterRoom);
-      ws.on("get-users", getUsers);
-      ws.on("user-disconnected", removePeer);
-      ws.on("user-started-sharing", (peerId) => setScreenSharingId(peerId));
-      ws.on("user-stopped-sharing", () => setScreenSharingId(""));
-      ws.on("name-changed", nameChangedHandler);
+    ws.on("room-created", enterRoom);
+    ws.on("get-users", getUsers);
+    ws.on("user-disconnected", removePeer);
+    ws.on("user-started-sharing", (peerId) => setScreenSharingId(peerId));
+    ws.on("user-stopped-sharing", () => setScreenSharingId(""));
+    ws.on("name-changed", nameChangedHandler);
 
-      return () => {
-          ws.off("room-created");
-          ws.off("get-users");
-          ws.off("user-disconnected");
-          ws.off("user-started-sharing");
-          ws.off("user-stopped-sharing");
-          ws.off("user-joined");
-          ws.off("name-changed");
-          me?.disconnect();
-      };
-      // eslint-disable-next-line react-hooks/exhaustive-deps
+    return () => {
+      ws.off("room-created");
+      ws.off("get-users");
+      ws.off("user-disconnected");
+      ws.off("user-started-sharing");
+      ws.off("user-stopped-sharing");
+      ws.off("user-joined");
+      ws.off("name-changed");
+      me?.disconnect();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
-      if (screenSharingId) {
-          ws.emit("start-sharing", { peerId: screenSharingId, roomId });
-      } else {
-          ws.emit("stop-sharing");
-      }
+    if (screenSharingId) {
+      ws.emit("start-sharing", { peerId: screenSharingId, roomId });
+    } else {
+      ws.emit("stop-sharing");
+    }
   }, [screenSharingId, roomId]);
 
   useEffect(() => {
-      if (!me) return;
-      if (!stream) return;
-      ws.on("user-joined", ({ peerId, userName: name }) => {
-          const call = me.call(peerId, stream, {
-              metadata: {
-                  userName
-              },
-          });
-          call.on("stream", (peerStream) => {
-              dispatch(addPeerStreamAction(peerId, peerStream));
-          });
-          dispatch(addPeerNameAction(peerId, name));
+    if (!me) return;
+    if (!stream) return;
+    ws.on("user-joined", ({ peerId, userName }) => {
+      const call = me.call(peerId, stream, {
+        metadata: {
+          userName,
+        },
       });
-
-      me.on("call", (call) => {
-          const { userName } = call.metadata;
-          dispatch(addPeerNameAction(call.peer, userName));
-          call.answer(stream);
-          call.on("stream", (peerStream) => {
-              dispatch(addPeerStreamAction(call.peer, peerStream));
-          });
+      call.on("stream", (peerStream) => {
+        dispatch(addPeerStreamAction(peerId, peerStream));
       });
+      dispatch(addPeerNameAction(peerId, userName));
+    });
 
-      return () => {
-          ws.off("user-joined");
-      };
+    me.on("call", (call) => {
+      const { userName } = call.metadata;
+      dispatch(addPeerNameAction(call.peer, userName));
+      call.answer(stream);
+      call.on("stream", (peerStream) => {
+        dispatch(addPeerStreamAction(call.peer, peerStream));
+      });
+    });
+
+    return () => {
+      ws.off("user-joined");
+    };
   }, [me, stream, userName]);
 
   return (
-      <RoomContext.Provider
-          value={{
-              stream,
-              screenStream,
-              peers,
-              shareScreen,
-              roomId,
-              setRoomId,
-              screenSharingId,
-          }}
-      >
-          {children}
-      </RoomContext.Provider>
+    <RoomContext.Provider
+      value={{
+        stream,
+        screenStream,
+        peers,
+        shareScreen,
+        roomId,
+        setRoomId,
+        screenSharingId,
+      }}
+    >
+      {children}
+    </RoomContext.Provider>
   );
 };
-
 
 // export const RoomContext = createContext({
 //   peers: {},
