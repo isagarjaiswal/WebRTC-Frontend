21c7fa7b6316a5452fa696cab36b673a486a8abe
isagarjaiwal <sagarjaiswal815555@gmail.com>
2024-03-21 18:43:07 +0530

    add 1111

--- src/ws.js
+++ src/ws.js
@@ -2,5 +2,6 @@ import socketIOClient from "socket.io-client";
 
 const WS = "https://webrtc-backend-1.onrender.com";
 // const WS = "http://localhost:8080/";
+const ws = socketIOClient(WS);
 
-export const ws = socketIOClient(WS);
+export default ws;
