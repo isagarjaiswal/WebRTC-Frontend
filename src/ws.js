ff009e68976e7b4b9482caceff9fee2f3ebfa329
isagarjaiwal <sagarjaiswal815555@gmail.com>
2024-03-21 17:06:20 +0530

    add custom room i1d

--- src/ws.js
+++ src/ws.js
@@ -2,4 +2,6 @@ import socketIOClient from "socket.io-client";
 
 const WS = "https://webrtc-backend-1.onrender.com";
 // const WS = "http://localhost:8080/";
+
 export const ws = socketIOClient(WS);
+ws.setMaxListeners(15);
